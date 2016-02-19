685945c65eb21c88fe1f37e01e780081bec8a3f0
dcmsy <43583189@qq.com>
2016-02-19 09:23:11 +0800

    first commit

---
+++ blog/src/component/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DatePicker } from 'antd';
+
+
+
+
+//时间选择器
+const App = React.createClass({
+  render() {
+    return (
+      <DatePicker />
+    );
+  },
+});
+
+
+
+export default App;
